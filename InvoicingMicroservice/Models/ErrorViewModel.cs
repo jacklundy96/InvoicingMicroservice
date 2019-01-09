6afbcd0163e20a9739e9003466f3f3bcaac8c358
Jack Lundy <jacklundy@hotmail.co.uk>
2019-01-09 19:01:28 +0000

    Initial Comit

---
+++ InvoicingMicroservice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InvoicingMicroservice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
